54a9f1566ce8f43ddd62629761ee1676c5f3a799
vincjo <v.joseph3@gmail.com>
2024-06-04 15:18:07 +0200

    fix: dropdown

--- src/lib/dropdown/Dropdown.svelte
+++ src/lib/dropdown/Dropdown.svelte
@@ -78,6 +78,9 @@
         if (keep) {
             return
         }
+        else if (contains) {
+            return active = false
+        }
         else if (preventClosing === true && leave === false) {
             return
         }
